7c59fcd24a63ff5435ce362ea34a382c55743c8a
ryan <ryan@tesseraktinstitute.org>
2023-10-03 23:02:34 -0700

    open up to login page

--- starcompanion/urls.py
+++ starcompanion/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("", LoginView.as_view(), name="login"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
